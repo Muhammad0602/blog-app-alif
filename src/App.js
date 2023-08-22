8b29afc98d981f823a7d495a89e3801c3214219c
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-08-22 19:15:36 +0500

    Add home and details pages to the app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Details from './components/Details';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Details/:id" element={<Details />} />
+      </Routes>
     </div>
   );
 }
